af9064a003fe73504b1c56c776cfdb2aea0385ca
Luca Bonamini <lucabonamini28@gmail.com>
2021-02-25 12:21:45 +0100

    Feat/docker (#3)
    * Fix install_dependencies.sh
    
    * Fix Dockerfile
    
    * Trying to get things work without rocker
    
    * Update Dockerfile
    
    * Testing
    
    * Docker working version

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt update
 sudo apt-get install --yes libeigen3-dev
 sudo apt-get install --yes libboost-all-dev
 sudo apt-get install --yes freeglut3-dev
@@ -6,14 +7,18 @@ sudo apt-get install --yes libglfw3-dev
 sudo apt-get install --yes libxinerama-dev
 sudo apt-get install --yes libxcursor-dev
 sudo apt-get install --yes libxi-dev
+sudo apt-get install --yes git
 
-mdkir external && cd external
+cd external
 
 # GLFW
 git clone -n https://github.com/glfw/glfw && cd glfw && git checkout 3.3.2 && cd ..
 
 # IMGUI
 git clone -n https://github.com/ocornut/imgui && cd imgui && git checkout fb7f6cab8c322731da336e553915e944bf386e62 && cd ..
+
+# IMPLOT
+git clone https://github.com/epezent/implot.git && cd ..
 
 echo [install_dependencies.sh] Leaving external/
 cd ..
